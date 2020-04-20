b66c63572f80bc519940f1c4eb926dc4ac1cff40
Evgeny Zislis <evgeny.zislis@gmail.com>
2020-04-20 23:22:06 +0000

    add an example for config.local.js

---
+++ config.local.example.js
@@ -0,0 +1,4 @@
+// change values and copy this file as 'config.local.js'
+const CHANNEL_ID = '<your channel/user number id>'
+const CLIENT_ID = '<the application client_id>'
+const CLIENT_REDIRECT_URI = 'https://<the application url & path>'
