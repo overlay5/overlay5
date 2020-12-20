8cd599e46cc9f2e7c890595a96d7afb2f06939a1
Evgeny Zislis <evgeny.zislis@gmail.com>
2020-12-20 18:01:32 +0000

    implement poor-mans green screen

--- components/webrtc-webcam.js
+++ components/webrtc-webcam.js
@@ -35,14 +35,50 @@
       updateWebcam: function () {
         if (this.webcamStream) {
           const webcamVideoNode = document.getElementById(this.id)
-          if (webcamVideoNode)
+          if (webcamVideoNode) {
             webcamVideoNode.srcObject = this.webcamStream
+            webcamVideoNode.addEventListener('play', () => {
+              this.timerCallback()
+            }, false)
+          }
         }
+      },
+      timerCallback: function () {
+        this.computeFrame()
+        setTimeout(() => this.timerCallback(), 3)
+      },
+      computeFrame: function () {
+        let canvasZ = document.getElementById(this.id + '-canvas-z')
+        let context = canvasZ.getContext('2d')
+        let video = document.getElementById(this.id)
+        canvasZ.width = video.videoWidth
+        canvasZ.height = video.videoHeight
+        context.drawImage(video, 0, 0, video.videoWidth/4, video.videoHeight/4)
+        let frame = context.getImageData(0, 0, video.videoWidth, video.videoHeight)
+        let l = frame.data.length / 4
+        function rgb2hsv(r,g,b) {
+          let v = Math.max(r,g,b), c = v - Math.min(r,g,b)
+          let h = c && ((v == r) ? (g-b)/c : ((b==g) ? 2 + (b-r)/c : 4 +(r-g)/c))
+          return [ 60*(h<0 ? h+6 : h), v && c/v, v]
+        }
+        for (let i = 0; i < l; i++) {
+          let hsv = rgb2hsv(frame.data[i*4+0],frame.data[i*4+1],frame.data[i*4+2])
+          if (hsv[0] == 180 && hsv[2] > 60 && hsv[2] < 260) {
+            frame.data[i*4+3] = 0
+          }
+        }
+        canvas = document.getElementById(this.id + '-canvas')
+        // canvas.width = video.videoWidth
+        canvas.height = video.videoHeight
+        canvas.getContext('2d').putImageData(frame, 0, 0)
+        return
       }
     },
     template: /*html*/`
-      <v-sheet :id="id + '-container'" elevation="3">
-        <video :id="id" autoplay />
+      <v-sheet color="transparent" :id="id + '-container'" elevation="0">
+        <video style="display:none" :id="id" autoplay />
+        <canvas style="display:none" :id="id + '-canvas-z'"/>
+        <canvas :id="id + '-canvas'"/>
       </v-sheet>
     `,
     activated: function() {
