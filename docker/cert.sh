3b0a31cb9522dadda16bd06cefe83f9a67063c39
Evgeny Zislis <evgeny.zislis@gmail.com>
2020-12-20 17:59:35 +0000

    prettier ssl cert printout on docker container boot

--- docker/cert.sh
+++ docker/cert.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-if [ ! -f /etc/ssl/overlay5.key -o ! -f /etc/ssl/overlay5.cert ]; then
+KEY_FILE=/etc/ssl/overlay5.key
+CERT_FILE=/etc/ssl/overlay5.cert
+
+if [ ! -f $KEY_FILE -o ! -f $CERT_FILE ]; then
     echo "Generating cert"
     openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
         -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=overlay5.local" \
-        -keyout /etc/ssl/overlay5.key -out /etc/ssl/overlay5.cert
+        -keyout $KEY_FILE -out $CERT_FILE
 fi
 
-echo "Certificate & key:"
-ls -altr /etc/ssl/
-exec openssl x509 -in /etc/ssl/overlay5.cert -noout -fingerprint -sha256 -text
+find /etc/ssl -type f -maxdepth 1 -ls
+exec openssl x509 -in $CERT_FILE \
+  -text \
+  -nocert \
+  -noout \
+  -certopt no_pubkey \
+  -certopt no_sigdump \
+  -certopt no_serial \
+  -certopt no_extensions
